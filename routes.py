f776bb068d631772dd28d8af2af6d0335556c8a3
unknown <romutaba@ymail.com>
2017-01-25 08:19:31 +0300

    add sessions, homepage function and login

--- routes.py
+++ routes.py
@@ -1,23 +1,22 @@
 from flask import * 
 from models import *
 from forms import *
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 user = User()
-
-@app.route('/')
-def index():
-	return render_template('index.html')
-
 
 # @app.route('/registration', methods=['POST', 'GET'])
 # def register():
-# 	fullname = 'Raymond John'
-# 	username = 'Ray'
-# 	password = 'password'
-# 	user.insert_data(fullname=fullname, username=username,password=password)
-# 	return "successfully inserted"
+#   fullname = 'Raymond John'
+#   username = 'Ray'
+#   password = 'password'
+#   user.insert_data(fullname=fullname, username=username,password=password)
+#   return "successfully inserted"
 
+
+#user sign up form using wtforms 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
@@ -29,8 +28,40 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	return 'login page'
+
+#user login form using wtforms 
+# @app.route('/index', methods=['GET', 'POST'])
+# def login():
+#     return 'login page'
+
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        #drop the session
+        session.pop('user', None)
+        if request.form['password'] == 'password':
+            session['user'] = request.form['username']
+            #create the landing page
+            return redirect(url_for('homepage'))
+
+    return render_template('index.html')
+
+
+@app.route('/homepage')
+def homepage():
+    #ensure user has session
+    if g.user:
+        return render_template('homepage.html')
+
+    return redirect(url_for('index'))
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+
 
 
